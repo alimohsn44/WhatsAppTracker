34bf3d5a62d13d71e830919983cf01439b948443
Ali Mohsn <mohsnali713@gmail.com>
2026-09-02 02:54:41 -0700

    Update app configuration

--- app.py
+++ app.py
@@ -17,10 +17,13 @@ app.config['SECRET_KEY'] = 'your-super-secret-key-change-this'
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.environ.get('DB_HOST', 'your-mysql-host'),
-        user=os.environ.get('DB_USER', 'your-username'),
-        password=os.environ.get('DB_PASSWORD', 'your-password'),
-        database=os.environ.get('DB_NAME', 'your-database')
+        host=os.environ.get('whatsapp-db-mohsnali713.c.aivencloud.com'),
+        user=os.environ.get('avnadmin'),
+        password=os.environ.get('AVNS_1O6ymCnv5tFbhuRurWk'),
+        database=os.environ.get('defaultdb'),
+        port=os.environ.get('27701'),
+        ssl_disabled=False,
+        use_pure=True
     )
 
 def init_db():
